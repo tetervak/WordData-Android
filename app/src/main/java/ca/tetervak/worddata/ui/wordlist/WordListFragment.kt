d70d46688cee91f39a23c7351856782370f23e91
Alex Tetervak <tetervak@sheridancollege.ca>
2022-10-02 15:26:12 -0400

    renamed FirstFragment to WordListFragment

--- app/src/main/java/ca/tetervak/worddata/ui/wordlist/WordListFragment.kt
+++ app/src/main/java/ca/tetervak/worddata/ui/wordlist/WordListFragment.kt
@@ -1,4 +1,4 @@
-package ca.tetervak.worddata
+package ca.tetervak.worddata.ui.wordlist
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,14 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import ca.tetervak.worddata.databinding.FragmentFirstBinding
+import ca.tetervak.worddata.R
+import ca.tetervak.worddata.databinding.FragmentWordListBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class WordListFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentWordListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,9 +23,10 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
         return binding.root
 
     }
